14a267bad63f9c1fd849fdc2ad833a899c58cb75
Diana <kisa@mail.ru>
2017-10-08 20:12:27 +0300

    add 3 and 4 homework

---
+++ src/ru/caf82/lectures/homework/Homework/src/homework/testclass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.caf82.lectures.diagramBomberman;
+
+
+public interface GameObject
+
+/**
+ *
+ * @author prask_000
+ */
+public class testclass {
+    
+}
---
+++ src/ru/caf82/lectures/homework/Homework/src/ru/caf82/result/machinelearning/models/KNeighbourhood.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.caf82.result.machinelearning;
+
+/**
+ *
+ * @author prask_000
+ */
+public class KNeighbourhood extends AbstractModel {
+    
+}
